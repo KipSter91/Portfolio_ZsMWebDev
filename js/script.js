c85aff8294a8514ada32a2148d6e5ecfcc953c7f
Zsolt Márku <zsolt.marku910221@gmail.com>
2023-07-09 13:11:20 +0200

    add typed javascript feature

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const typed= new Typed(".text", {
+    strings:["Full-stack web developer" , "Warehouse Manager/Supervisor" ,"Traveller", "Play Station 5 Fanatic... :)"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 2000,
+    loop: true
+});
