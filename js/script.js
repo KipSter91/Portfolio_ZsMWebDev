9a3b07e5999b11469b2e79f5f4b0a5b00da98c19
Zsolt Marku <zsolt.marku910221@gmail.com>
2023-07-22 18:00:43 +0200

    add dark/light mode toggle

--- js/script.js
+++ js/script.js
@@ -48,8 +48,16 @@ ScrollReveal({
     reset: true,
     distance: "80px",
     duration: 2000,
-    delay: 200 
+    delay: 200
 });
-ScrollReveal().reveal('.home-content, .heading, .contact', { origin: "top"});
-ScrollReveal().reveal('.home-content h1', { origin: "left"});
+ScrollReveal().reveal('.home-content, .heading', { origin: "top" });
+ScrollReveal().reveal('.home-content h1', { origin: "left" });
 ScrollReveal().reveal('.skills-column, .about-content, .about-img, .home-img, .portfolio-container, .contact', { origin: "bottom" });
+
+//Toggle dark/light mode
+
+const toggleMode = document.getElementById("checkbox");
+
+checkbox.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+})
