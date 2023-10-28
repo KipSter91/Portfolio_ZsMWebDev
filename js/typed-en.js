c6773f6c80bad612f6cb370b30143277be2ca2b2
Zsolt Marku <zsolt.marku910221@gmail.com>
2023-10-28 17:48:00 +0200

    format the rest also with prettier

--- js/typed-en.js
+++ js/typed-en.js
@@ -1,8 +1,14 @@
 // typed feature for home section (EN)
 const typed = new Typed(".text", {
-    strings: ["Warehouse Supervisor", "Full-stack Webdeveloper", "Husband (of Barbara Márku)", "Traveller", "Video Game Fanatic... :)"],
-    typeSpeed: 50,
-    backSpeed: 50,
-    backDelay: 1000,
-    loop: true
+  strings: [
+    "Warehouse Supervisor",
+    "Full-stack Webdeveloper",
+    "Husband (of Barbara Márku)",
+    "Traveller",
+    "Video Game Fanatic... :)",
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  backDelay: 1000,
+  loop: true,
 });
--- js/typed-nl.js
+++ js/typed-nl.js
@@ -1,8 +1,14 @@
 // typed feature for home section (NL)
 const typed = new Typed(".text", {
-    strings : ["Warehouse Supervisor", "full-stack webontwikkelaar", "echtgenoot (van Barbara Márku)", "wereldreiziger", "videospel fanatiek... :)"],
-    typeSpeed: 50,
-    backSpeed: 50,
-    backDelay: 1000,
-    loop: true
+  strings: [
+    "Warehouse Supervisor",
+    "full-stack webontwikkelaar",
+    "echtgenoot (van Barbara Márku)",
+    "wereldreiziger",
+    "videospel fanatiek... :)",
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  backDelay: 1000,
+  loop: true,
 });
